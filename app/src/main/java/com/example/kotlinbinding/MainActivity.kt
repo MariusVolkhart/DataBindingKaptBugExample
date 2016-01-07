4668ee3f279890e6024c7d2220176cea67a20de4
Marius Volkhart <Marius@volkhart.com>
2016-01-07 11:25:32 -0600

    Sources for the bug

---
+++ app/src/main/java/com/example/kotlinbinding/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinbinding
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.kotlinbinding.databinding.ActivityMainBinding
+
+public class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
